da405d0f7dcc8d7333d6ae9616ab2cc0c660ff4d
m0lecularman <topher.l.coleman@gmail.com>
2023-06-14 20:14:24 -0400

    dying

---
+++ codingchallenges/301OC5.py
@@ -0,0 +1,31 @@
+# Script: 301OC5
+# Author: toph
+# Date: 14JUN23
+
+#Part 1
+
+#Write a program that works out whether if a given number is an even or odd number
+
+#Start
+
+number = int(input("Pick a number my G"))
+if (number % 2) == 0:
+    print(f"This numerical joint even, fam.")
+else:
+    print(f"That number odd, bruh.")
+
+#End Part 1
+
+#Part 2
+
+#Write a program that gives you an existential crisis
+
+#Start
+
+age = int(input("How old you is?"))
+x = (90 - age) * 365
+y = (90 - age) * 52
+z = (90 - age) * 12
+print(f"Aight playboi, you got {x} days, {y} months, n {z} years left...at least, if you ain't smoked all them Newports, you would.")
+
+#End
